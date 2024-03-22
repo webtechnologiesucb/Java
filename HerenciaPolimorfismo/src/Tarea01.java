6140abf54e47551969cb94adbfbe5cfdc77e83b5
webtechnologiesucb <miguel.pacheco@ucb.edu.bo>
2024-03-22 19:13:55 -0400

    compelto

---
+++ HerenciaPolimorfismo/src/Tarea01.java
@@ -0,0 +1,67 @@
+import java.util.Random;
+
+class Dado {
+    public int lanzar() {
+        Random rand = new Random();
+        return rand.nextInt(6) + 1;
+    }
+}
+
+class Juego {
+    private Dado dado1;
+    private Dado dado2;
+
+    public Juego(Dado dado1, Dado dado2) {
+        this.dado1 = dado1;
+        this.dado2 = dado2;
+    }
+
+    public boolean jugar() {
+        int suma = dado1.lanzar() + dado2.lanzar();
+        return suma == 7;
+    }
+}
+
+class Simulador {
+    private int numJuegos;
+    private int juegosGanados;
+    private int juegosPerdidos;
+    private Dado dado1;
+    private Dado dado2;
+
+    public Simulador(int numJuegos) {
+        this.numJuegos = numJuegos;
+        this.juegosGanados = 0;
+        this.juegosPerdidos = 0;
+        this.dado1 = new Dado();
+        this.dado2 = new Dado();
+    }
+
+    public void simular() {
+        for (int i = 0; i < numJuegos; i++) {
+            Juego juego = new Juego(dado1, dado2);
+            if (juego.jugar()) {
+                juegosGanados++;
+            } else {
+                juegosPerdidos++;
+            }
+        }
+    }
+
+    public void mostrarResultados() {
+        int totalJuegos = juegosGanados + juegosPerdidos;
+        double porcentajeGanados = ((double) juegosGanados / totalJuegos) * 100;
+        double porcentajePerdidos = ((double) juegosPerdidos / totalJuegos) * 100;
+        System.out.println("Resultados de la simulación:");
+        System.out.println("Partidas ganadas: " + juegosGanados + " (" + String.format("%.2f", porcentajeGanados) + "%)");
+        System.out.println("Partidas perdidas: " + juegosPerdidos + " (" + String.format("%.2f", porcentajePerdidos) + "%)");
+    }
+}
+
+public class Tarea01 {
+    public static void main(String[] args) {
+        Simulador simulador = new Simulador(10);
+        simulador.simular();
+        simulador.mostrarResultados();
+    }
+}
---
+++ HerenciaPolimorfismo/src/Tarea02.java
@@ -0,0 +1,97 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+abstract class Juego01 {
+    protected List<Integer> secuencia;
+
+    public Juego01() {
+        secuencia = new ArrayList<>();
+        for (int i = 1; i <= 9; i++) {
+            secuencia.add(i);
+        }
+        Collections.shuffle(secuencia);
+        secuencia = secuencia.subList(0, 4);
+    }
+
+    public abstract int obtenerFamas(List<Integer> intento);
+    public abstract int obtenerToques(List<Integer> intento);
+    public abstract boolean esVictoria(int famas);
+    public abstract String mensajeVictoria();
+
+    public List<Integer> getSecuencia() {
+        return secuencia;
+    }
+}
+
+class ToqueYFamaJuego extends Juego01 {
+    public ToqueYFamaJuego() {
+        super();
+    }
+
+    @Override
+    public int obtenerFamas(List<Integer> intento) {
+        int famas = 0;
+        for (int i = 0; i < 4; i++) {
+            if (secuencia.get(i).equals(intento.get(i))) {
+                famas++;
+            }
+        }
+        return famas;
+    }
+
+    @Override
+    public int obtenerToques(List<Integer> intento) {
+        int toques = 0;
+        for (int num : intento) {
+            if (secuencia.contains(num) && !(secuencia.indexOf(num) == intento.indexOf(num))) {
+                toques++;
+            }
+        }
+        return toques;
+    }
+
+    @Override
+    public boolean esVictoria(int famas) {
+        return famas == 4;
+    }
+
+    @Override
+    public String mensajeVictoria() {
+        return "¡Felicidades! Has adivinado la secuencia correctamente.";
+    }
+}
+
+public class Tarea02 {
+    public static void main(String[] args) {
+    	ToqueYFamaJuego juego = new ToqueYFamaJuego();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bienvenido al juego Toque y Fama!");
+        System.out.println("Intenta adivinar la secuencia de 4 números diferentes entre 1 y 9.");
+
+        int intentos = 0;
+        while (true) {
+            intentos++;
+            System.out.println("Intento #" + intentos + ":");
+            List<Integer> intento = new ArrayList<>();
+            for (int i = 0; i < 4; i++) {
+                System.out.print("Ingrese el número " + (i + 1) + ": ");
+                int num = scanner.nextInt();
+                intento.add(num);
+            }
+
+            int famas = juego.obtenerFamas(intento);
+            int toques = juego.obtenerToques(intento);
+
+            System.out.println("Famas: " + famas);
+            System.out.println("Toques: " + toques);
+
+            if (juego.esVictoria(famas)) {
+                System.out.println(juego.mensajeVictoria());
+                break;
+            }
+        }
+        scanner.close();
+    }
+}
